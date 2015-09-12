96f9f85a8ce9f8de53457b57e718f015e4ab74dd
Abhimanyu Jain <abhimanyu.jain@flipkart.com>
2015-09-12 12:54:37 +0530

    landing pages

--- app/controllers/generic_orders_controller.rb
+++ app/controllers/generic_orders_controller.rb
@@ -2,21 +2,44 @@ class GenericOrdersController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
+  @@img_src = Hash.new
+  @@img_src = {
+      "Packers And Movers" => {"img" => "11.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Electrical" => {"img" => "1.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Plumbing" => {"img" => "2.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Car Wash" => {"img" => "3.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Laundry" => {"img" => "4.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Computer Repair" => {"img" => "7.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Appliance Repair" => {"img" => "8.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Pest Control" => {
+          "img" => "9.png",
+          "tagline" => "We perfectly understand how pests can make life truly miserable. Whether you are a business institution, home, hospital or any other place, we offer you timely and expert help."
+      },
+      "House Cleaning" => {"img" => "10.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Keymaker" => {"img" => "key maker1.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Carpenter" => {"img" => "carpenter.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Cook" => {"img" => "5.png", "tagline" => "We help you move, 24x7, 365 days a year. Contact us to get rid of all your moving worries."},
+      "Painter" => {
+          "img" => "paint.png",
+          "tagline" => "Our professional painters are experts. You can count on our fully vetted painters to come prepared with all the proper tools to get the job done right the first time."
+      }
+  }
+
   def index
     if session[:city]
       @city = session[:city]
     end
-	end
+  end
 
   def new
   end
 
   def create
-    @order =  Order.new(order_params)
+    @order = Order.new(order_params)
     if @order[:origin_of_request] == nil
       @order[:origin_of_request] = @@src
     end
-    @order[:service] = @@service
+    @order[:service] = cookies[:service]
     @order[:initiated_on] = DateTime.now
     @order[:rating] = 0
     @order[:feedback] = 'nil'
@@ -52,8 +75,8 @@ class GenericOrdersController < ApplicationController
   end
 
   def select_service_page
-    @@service = params[:service_name]
-    redirect_to URI.escape('/'+ @@service)
+    cookies[:service] = params[:service_name]
+    redirect_to URI.escape('/'+ cookies[:service])
   end
 
   def personal
@@ -63,49 +86,20 @@ class GenericOrdersController < ApplicationController
     @@coupon_code = params[:coupon_code]
 
     if browser.mobile?
-      @@src =  "mobile browser"
+      @@src = "mobile browser"
     else
       @@src = "desktop browser"
     end
   end
 
-  def plumbing
-  end
-
-  def electrical
-  end
-
-  def house_cleaning
-  end
-
-  def keymaker
-  end
-
-  def laundry
-  end
-
-  def movers_packers
-  end
-
-  def painter
-  end
-
-  def pest_control
-  end
-
-  def carpenter
-  end
-
-  def car_wash
-  end
-
-  def computer_repair
-  end
-
-  def appliance_repair
-  end
-
-  def cook
+  def orderInfoForm
+    temp = URI.decode(request.env['PATH_INFO'])
+    temp.slice!(0)
+    if cookies[:service] != temp
+      cookies[:service] = temp
+    end
+    @img = @@img_src[cookies[:service]]["img"]
+    @tag_line = @@img_src[cookies[:service]]["tagline"]
   end
 
   def set_city
@@ -116,10 +110,10 @@ class GenericOrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:customer_id, :service, :booking_date,:booking_slot,:customer_comments, :name, :address, :phone, :email)
+    params.permit(:customer_id, :service, :booking_date, :booking_slot, :customer_comments, :name, :address, :phone, :email)
   end
 
-  #@@gurgaon_services = Array.new("Electrical", "Plumbing", "Packers and Movers", "Car Wash", "Pest Control", "Computer Repair", "Appliance Repair", "House Cleaning", "Carpenter", "Cook", "Painter", "Keymaker")
+  #gurgaon_services = Array.new("Electrical", "Plumbing", "Packers and Movers", "Car Wash", "Pest Control", "Computer Repair", "Appliance Repair", "House Cleaning", "Carpenter", "Cook", "Painter", "Keymaker")
   #@@bangalore_services = Array.new("Electrical", "Plumbing", "Packers and Movers", "Car Wash", "Pest Control", "Computer Repair", "Appliance Repair", "House Cleaning", "Carpenter", "Cook", "Painter", "Keymaker")
 
 
