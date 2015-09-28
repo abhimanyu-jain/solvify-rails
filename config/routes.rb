21d32135aa05e729d1a49e23dd302494639ac6f8
Abhimanyu Jain <abhimanyu.jain@flipkart.com>
2015-09-28 23:54:36 +0530

    facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Rails.application.routes.draw do
       :omniauth_callbacks => "users/omniauth_callbacks"
   }
 
-  
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
